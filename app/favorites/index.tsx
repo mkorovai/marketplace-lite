3dd8800c07a0139a27cb2f60f604a9c808440e56
mkorovai <mykhailo.korovai@gmail.com>
2026-03-06 13:20:08 +0200

    feat: add empty states

--- app/favorites/index.tsx
+++ app/favorites/index.tsx
@@ -2,10 +2,22 @@
 import { StyleSheet, ViewStyle, TextStyle, ImageStyle } from 'react-native';
 
 // components
+import EmptyState from '@/components/ui/EmptyState';
 import ThemedView from '@/components/ui/ThemedView';
 import ThemedText from '@/components/ui/ThemedText';
 
-export default function FavoritesScreen() {
+// types
+import { Product } from '@/types/product';
+
+type Props = {
+  products: Product[];
+};
+
+export default function FavoritesScreen({ products }: Props) {
+  if (!products.length) {
+    return <EmptyState title="No favorites yet" description="Tap ❤️ on products you like" />;
+  }
+
   return (
     <ThemedView style={styles.root}>
       <ThemedText>Favorites</ThemedText>
